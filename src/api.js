5ebc3f12b26836414983e69d2a884db35a5c367e
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2024-07-12 15:17:57 +0300

    сделала api отдельно

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 const  ACCESS_KEY = "hz6DtNu86WDq2eHwrs231CdhsA0ERXri0mKKnpcZtbI";
 axios.defaults.baseURL="https://api.unsplash.com"
-export const fetchArticles = async(serchtopic, page=1) =>{
+export const fetchArticles = async(serchtopic, page=1) => {
 const response = await axios.get("/search/photos", {
     params:{
         query: serchtopic,
         page: page,
-        client_id:  ACCESS_KEY
+        client_id:  ACCESS_KEY,
 
-    },
-    return: response.data.results
-})}
+    },})
+    return response.data.results
+}
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,11 @@ import SearchBar from "../SearchBar/SearchBar";
 import ImageGallerry from "../ImageGallery/ImageGallery";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
+import { fetchArticles } from "../../api";
 
 export default function App() {
   const [photos, setPhotos] = useState([]);
-  const [searchTopic, setSearchTopic] = useState([]);
+  const [searchTopic, setSearchTopic] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -15,25 +16,28 @@ export default function App() {
     setSearchTopic(value);
   };
 
+  useEffect(()=>{
   
-  useEffect(() => {
-    
-    async function fetchArticles() {
-      try{
-        setLoading(true);
-        setError(false)
-      const response = await axios.get(
-        `https://api.unsplash.com/search/photos?page=${page}&query=${searchTopic}&client_id=${KEY}`
-      );
-      setPhotos(response.data.results);} catch (error){
-        setError(true);
-        
-
-      }finally{setLoading(false);}
+    async function getPhotos(){
+      if (searchTopic === ""){
+        return
+       }
+    try{
+      setLoading(true);
+      const data = await fetchArticles(searchTopic);
+      console.log(data);
+      setPhotos(data)
+      console.log(setPhotos)
+    } catch (error){
+      setError(true);
+    } finally{
+      setLoading(false)
+    }}
+    if (searchTopic) {
+      getPhotos();
     }
+  }, [searchTopic])
 
-    fetchArticles();
-  }, [searchTopic]);
 
   return (
     <div>
